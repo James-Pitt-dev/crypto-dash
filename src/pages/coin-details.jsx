835860d7739971d9d8e642e86bbf1a973f393ee7
James Pitt <39842510+James-Pitt-dev@users.noreply.github.com>
2026-07-21 13:16:07 -0700

    Added ChartJS component

--- src/pages/coin-details.jsx
+++ src/pages/coin-details.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
-const VITE_COIN_API_URL = import.meta.VITE_COIN_API_URL;
+import { Link, useParams } from "react-router";
+import Spinner from "../components/Spinner";
+import CoinChart from "../components/CoinChart";
+const VITE_COIN_API_URL = import.meta.env.VITE_COIN_API_URL;
 
 
 const CoinDetailsPage = () => {
@@ -16,10 +19,9 @@ const CoinDetailsPage = () => {
                 const res = await fetch(`${VITE_COIN_API_URL}/${id}`);
                 if (!res.ok) throw new Error('Failed to fetch data');
                 const data = await res.json();
-                console.log(data);
+                console.log(data)
                 setCoin(data);
             } catch (err) {
-                console.log(err);
                 setError(err.message)
             } finally {
                 setLoading(false);
@@ -28,7 +30,52 @@ const CoinDetailsPage = () => {
         fetchCoin();
     }, [id])
 
-    return ( <div className="">Coin Details {id}</div> );
+    return ( 
+        <div className="coin-details-container">
+            <Link to='/'>⬅️ Back To Home</Link>
+            <h1 className="coin-details-title">
+                {coin ? `${coin.name} (${coin.symbol.toUpperCase()})` : 'Coin Details' }
+            </h1>
+            {loading && <Spinner />}
+            {error && <div className='error'>❌{error}</div>}
+
+            {!loading && !error && (
+                <>
+                    <img 
+                        src={coin.image.large}
+                        alt={coin.name}
+                        className="coin-details-image"
+                    />
+                    <p>{coin.description.en.split('. ')[0] + '.'}</p>
+                    <div className="coin-details-info">
+                        <h3>Rank: #{coin.market_cap_rank}</h3>
+                        <h3>
+                            Current Price: ${coin.market_data.current_price.usd.toLocaleString()}
+                        </h3>
+                        <h4>
+                            Market Cap: ${coin.market_data.market_cap.usd.toLocaleString()}
+                        </h4>
+                        <h4>
+                            24H High: ${coin.market_data.high_24h.usd.toLocaleString()}
+                        </h4>
+                        <h4>
+                            24H Low: ${coin.market_data.low_24h.usd.toLocaleString()}
+                        </h4>
+                        <h4>
+                            24H Price Change: {coin.market_data.price_change_percentage_24h.toFixed(2)}%
+                        </h4>
+                        <h4>
+                            All-Time High: ${coin.market_data.ath.usd.toLocaleString()} on{' '}
+                            {new Date(coin.market_data.ath_date.usd).toLocaleDateString()}
+                        </h4>
+                    </div>
+
+                    <CoinChart coinId={coin.id}/>
+                </>
+            )}
+            {!loading && !error && !coin && <p>No Data Found!</p>}
+        </div>
+     );
 }
  
 export default CoinDetailsPage;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import CoinCard from '../components/CoinCard';
 import LimitSelector from '../components/LimitSelector';
 import FilterInput from '../components/FilterInput';
 import SortSelector from '../components/SortSelector';
+import Spinner from '../components/Spinner';
 
 const HomePage = ({
     coins, filter, setFilter, limit, setLimit, sortBy, setSortBy, loading, error
@@ -38,7 +39,7 @@ const HomePage = ({
       <h1>
         🚀 Crypto Dash
       </h1>
-      {loading && <p>Loading ...</p>}
+      {loading && <Spinner color='white'/>}
       {error && <div className='error'>{error}</div>}
 
       <div className="top-controls">
